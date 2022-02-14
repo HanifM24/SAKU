5e01b54d7b628ca50c568e3a688d2a9cfb47b17f
Hanif Muhammad <HanifMuh24@gmail.com>
2022-02-14 13:15:26 +0700

    commit 1

---
+++ src/main/java/SINDUPAN/SAKU/Model/GetListTRXModel.java
@@ -0,0 +1,112 @@
+package SINDUPAN.SAKU.Model;
+
+public class GetListTRXModel{
+    public String NO_TRXDBT;
+    public String NO_TRXKDT;
+    public String NO_COA_DBT;
+    public String NO_COA_KDT;
+    public String MATA_UANG_DBT;
+    public String MATA_UANG_KDT;
+    public String INVOICE_DBT;
+    public String INVOICE_KDT;
+    public Double NOMINALTRXDBT;
+    public Double NOMINALTRXKDT;
+    public String KTRG_DBT;
+    public String KTRG_KDT;
+
+    public String getNO_TRXDBT() {
+        return NO_TRXDBT;
+    }
+
+    public String getNO_TRXKDT() {
+        return NO_TRXKDT;
+    }
+
+    public void setNO_TRXKDT(String NO_TRXKDT) {
+        this.NO_TRXKDT = NO_TRXKDT;
+    }
+
+    public void setNO_TRXDBT(String NO_TRXDBT) {
+        this.NO_TRXDBT = NO_TRXDBT;
+    }
+
+    public String getNO_COA_DBT() {
+        return NO_COA_DBT;
+    }
+
+    public void setINVOICE_KDT(String INVOICE_KDT) {
+        this.INVOICE_KDT = INVOICE_KDT;
+    }
+
+    public String getINVOICE_KDT() {
+        return INVOICE_KDT;
+    }
+
+    public String getMATA_UANG_DBT() {
+        return MATA_UANG_DBT;
+    }
+
+    public void setINVOICE_DBT(String INVOICE_DBT) {
+        this.INVOICE_DBT = INVOICE_DBT;
+    }
+
+    public String getINVOICE_DBT() {
+        return INVOICE_DBT;
+    }
+
+    public String getMATA_UANG_KDT() {
+        return MATA_UANG_KDT;
+    }
+
+    public String getNO_COA_KDT() {
+        return NO_COA_KDT;
+    }
+
+    public String getKTRG_DBT() {
+        return KTRG_DBT;
+    }
+
+    public void setMATA_UANG_DBT(String MATA_UANG_DBT) {
+        this.MATA_UANG_DBT = MATA_UANG_DBT;
+    }
+
+    public Double getNOMINALTRXDBT() {
+        return NOMINALTRXDBT;
+    }
+
+    public Double getNOMINALTRXKDT() {
+        return NOMINALTRXKDT;
+    }
+
+    public void setKTRG_DBT(String KTRG_DBT) {
+        this.KTRG_DBT = KTRG_DBT;
+    }
+
+    public String getKTRG_KDT() {
+        return KTRG_KDT;
+    }
+
+    public void setKTRG_KDT(String KTRG_KDT) {
+        this.KTRG_KDT = KTRG_KDT;
+    }
+
+    public void setMATA_UANG_KDT(String MATA_UANG_KDT) {
+        this.MATA_UANG_KDT = MATA_UANG_KDT;
+    }
+
+    public void setNO_COA_DBT(String NO_COA_DBT) {
+        this.NO_COA_DBT = NO_COA_DBT;
+    }
+
+    public void setNO_COA_KDT(String NO_COA_KDT) {
+        this.NO_COA_KDT = NO_COA_KDT;
+    }
+
+    public void setNOMINALTRXDBT(Double NOMINALTRXDBT) {
+        this.NOMINALTRXDBT = NOMINALTRXDBT;
+    }
+
+    public void setNOMINALTRXKDT(Double NOMINALTRXKDT) {
+        this.NOMINALTRXKDT = NOMINALTRXKDT;
+    }
+}
